ec0ca1db8c4455191af399965148df544aad6756
bashtavenko <stanbpublic@live.com>
2025-10-23 09:59:16 -0400

    Save frames

--- image_processing_toolbox.cc
+++ image_processing_toolbox.cc
@@ -51,4 +51,52 @@ absl::Status SaveFrames(absl::string_view video_path, int32_t k,
   }
   return absl::OkStatus();
 }
-}  // namespace sfm
+
+absl::StatusOr<MotionSummary> ComputeMotionSummary(
+    const cv::Mat& image_frame, const cv::Mat& previous_image_frame) {
+  MotionSummary motion_summary;
+
+  std::vector<cv::Point2f> corners_a;
+  std::vector<cv::Point2f> corners_b;
+  constexpr int32_t kMaxCorners = 500;
+  cv::goodFeaturesToTrack(previous_image_frame, // Image to track
+                          corners_a, // Vector of detected corners (output)
+                          kMaxCorners, // Keep up to this many corners
+                          0.01, // Quality level (percent of maximum)
+                          5, // Min distance between corners
+                          cv::noArray(), // Mask
+                          3, // Block size
+                          false, // true: Harris, false: Shi-Tomasi
+                          0.04 // method specific parameter
+      );
+
+  constexpr int32_t win_size = 10; // compute local coherent motion
+  cv::cornerSubPix(
+      previous_image_frame, // Input image
+      corners_a, // Vector of corners (input and output)
+      cv::Size(win_size, win_size), // Half side length of search window
+      cv::Size(-1, -1), // Half side length of dead zone (-1=none)
+      cv::TermCriteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS,
+                       20, // Maximum number of iterations
+                       0.03 // Minimum change per iteration
+          ));
+
+  // Lucas Kanade algorithm
+  std::vector<uchar> features_found;
+  cv::calcOpticalFlowPyrLK(
+      previous_image_frame,           // Previous image
+      image_frame,           // Next image
+      corners_a,       // Previous set of corners (from imgA)
+      corners_b,       // Next set of corners (from imgB)
+      features_found,  // Output vector, each is 1 for tracked
+      cv::noArray(),   // Output vector, lists errors (optional)
+      cv::Size(win_size * 2 + 1, win_size * 2 + 1),  // Search window size
+      5,  // Maximum pyramid level to construct
+      cv::TermCriteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS,
+                       20,  // Maximum number of iterations
+                       0.3  // Minimum change per iteration
+                       ));
+
+  return motion_summary;
+}
+} // namespace sfm
--- image_processing_toolbox.h
+++ image_processing_toolbox.h
@@ -3,15 +3,49 @@
 #include <filesystem>
 #include "absl/status/status.h"
 #include "absl/strings/string_view.h"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include "absl/status/statusor.h"
+#include "opencv2/opencv.hpp"
 
 namespace sfm {
-
 // Saves k frames from the given video into output directory
 absl::Status SaveFrames(absl::string_view video_path, int32_t k,
                         absl::string_view frame_output_directory);
 
-}  // namespace sfm
+// Finds quality frames for structure from motion.
+// Frames should have different camera poses and have overlap.
+// Depending on criteria the number of output frames varies and can zero.
+absl::Status SaveQualityFrames(absl::string_view video_path,
+                        absl::string_view frame_output_directory);
+
+struct MotionSummary {
+  enum class MotionType {
+    // Very low motion magnitude (< 1.0 pixels)
+    kMinimal,
+    // Low standard deviation in flow components, indicating uniform motion
+    // (translation, rotation, or scaling)
+    kRigid,
+    // High variation in flow field, suggesting non-rigid or complex motion
+    kDeformative
+  };
+  float mean_keypoint_dx_pixels;
+  float mean_keypoint_dy_pixels;
+  float previous_keypoint_size;
+  // Mean motion by dx;
+  float mean_dx;
+  // Mean motion by dy;
+  float mean_dy;
+  // Mean magnitude (sqrt(dx^2 + dy^2)
+  float mean_magnitude;
+  // Mean direction in degrees.
+  float mean_direction;
+  float std_dx;
+  float std_dy;
+  MotionType motion_type;
+};
+
+absl::StatusOr<MotionSummary>ComputeMotionSummary(cv::Mat& image_frame, cv::Mat& previous_image_frame);
+
+
+} // namespace sfm
 
 #endif  // IMAGE_PROCESSING_TOOLBOX_H
