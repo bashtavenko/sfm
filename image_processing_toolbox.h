cca1bd1c1994058873e080064bccaef1416ea420
bashtavenko <stanbpublic@live.com>
2025-10-24 09:18:01 -0400

    SIFT start

--- image_processing_toolbox.h
+++ image_processing_toolbox.h
@@ -1,6 +1,9 @@
 #ifndef IMAGE_PROCESSING_TOOLBOX_H
 #define IMAGE_PROCESSING_TOOLBOX_H
 #include <filesystem>
+#include "absl/status/statusor.h"
+#include "opencv2/features2d.hpp"
+
 #include "absl/status/status.h"
 #include "absl/strings/string_view.h"
 #include "opencv2/highgui.hpp"
@@ -11,6 +14,12 @@ namespace sfm {
 // Saves k frames from the given video into output directory
 absl::Status SaveFrames(absl::string_view video_path, int32_t k,
                         absl::string_view frame_output_directory);
+
+absl::StatusOr<std::vector<int32_t>> MatchFeatures(const cv::Ptr<cv::Feature2D>& detector,
+  const cv::Ptr<cv::DescriptorMatcher>& matcher, const cv::Mat& image,
+  const cv::Mat& previous_image,
+  float ratio_threshold = 0.75f);
+
 
 }  // namespace sfm
 
